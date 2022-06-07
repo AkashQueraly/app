0b48245c05e5685ee3a6cb7068b8c734f9086d6b
AkashQueraly <aakashqueraly@gmail.com>
2022-06-07 13:03:39 +0530

    7jun

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function Greeting() {
-  return <h2>this is akash and this is my first component</h2>;
+// CSS
+import'./index.css';
+function Booklist() {
+  return (
+    <section className="booklist">
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
-ReactDOM.render(<Greeting />, document.getElementById('root'));
- 
+const Book = () => {
+  return  <article className="book">
+    <Image></Image>
+    <Title/>
+    <Author/>
+  </article>;
+};
+
+const Image = () => (
+  <img
+    src="https://images-eu.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX198_BO1,204,203,200_QL40_FMwebp_.jpg"
+    alt=""
+  />
+);
+
+const Title = () => <h1>I Love You to the Moon and Back</h1>
+const Author = () => <h4>Amelia Hepworth</h4>
+
+ReactDOM.render(<Booklist />, document.getElementById("root"));
