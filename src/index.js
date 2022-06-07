f7938c774e57ebc467653ab144536da06e6c42d9
AkashQueraly <aakashqueraly@gmail.com>
2022-06-07 15:17:25 +0530

    7jun

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // CSS
-import'./index.css';
+import "./index.css";
+// setup vars
+const author = "Amelia Hepworth";
+const title = "I Love You to the Moon and Back";
+const img =
+  "https://images-eu.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX198_BO1,204,203,200_QL40_FMwebp_.jpg";
 function Booklist() {
   return (
     <section className="booklist">
@@ -19,22 +24,17 @@ function Booklist() {
   );
 }
 
-const Book = () => {
-  return  <article className="book">
-    <Image></Image>
-    <Title/>
-    <Author/>
-  </article>;
+const Book = (props) => {
+  console.log(props);
+  
+  return (
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    {console.log(props)}
+    </article>
+  );
 };
-
-const Image = () => (
-  <img
-    src="https://images-eu.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX198_BO1,204,203,200_QL40_FMwebp_.jpg"
-    alt=""
-  />
-);
-
-const Title = () => <h1>I Love You to the Moon and Back</h1>
-const Author = () => <h4>Amelia Hepworth</h4>
 
 ReactDOM.render(<Booklist />, document.getElementById("root"));
