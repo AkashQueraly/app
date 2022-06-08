f7b07c00940fe4e938b0eb2b2504c92dd31f39eb
AkashQueraly <aakashqueraly@gmail.com>
2022-06-08 14:09:50 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,51 +3,52 @@ import ReactDOM from "react-dom";
 
 // CSS
 import "./index.css";
-// setup vars
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL200_SR200,200_.jpg",
+// setup vars{
+  const books = [
+{  img: 
+  "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL200_SR200,200_.jpg",
   title: "I Love You to the Moon and Back",
   author: "Amelia Hepworth",
-};
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL200_SR200,200_.jpg",
+},
+{
+  img: 
+  "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL200_SR200,200_.jpg",
   title:
     "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
   author: "James Clear",
-};
+},
+{
 
+    img: 
+    "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
+    title:
+      "If Animals Kissed Good Night",
+    author: "Ann Whitford Paul",
+  },
+
+
+  ];
 function Booklist() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, nisi
-        explicabo at veritatis molestiae suscipit ducimus sunt reiciendis
-        temporibus nihil reprehenderit voluptate dicta, ratione adipisci dolor.
-        Fugiat distinctio quia iusto?
-      </p>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book)=>{
+        const {img,title,author}=book;
+        return (
+          <Book book={book}></Book>
+        )
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
-  const {img,title, author} = props;
-  console.log(props)
+  console.log(props);
+  const {img,title, author,} = props.book;
   return (
     <article className="book">
       <img src={img} alt="" />
        <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children} 
     </article>
   );
 };
