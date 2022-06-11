204d3e61a8347a26c6bfa8dac320d65bbe667459
AkashQueraly <aakashqueraly@gmail.com>
2022-06-11 20:04:49 +0530

    initial commit

---
+++ src/books.js
@@ -0,0 +1,21 @@
+export const books = [
+    {
+      id: 1,
+      img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL200_SR200,200_.jpg",
+      title: "I Love You to the Moon and Back",
+      author: "Amelia Hepworth",
+    },
+    {
+      id: 2,
+      img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL200_SR200,200_.jpg",
+      title:
+        "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
+      author: "James Clear",
+    },
+    {
+      id: 3,
+      img: "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
+      title: "If Animals Kissed Good Night",
+      author: "Ann Whitford Paul",
+    },
+  ];
--- src/index.js
+++ src/index.js
@@ -3,67 +3,20 @@ import ReactDOM from "react-dom";
 
 // CSS
 import "./index.css";
-// setup vars{
-const books = [
-  {
-    id: 1,
-    img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL200_SR200,200_.jpg",
-    title: "I Love You to the Moon and Back",
-    author: "Amelia Hepworth",
-  },
-  {
-    id: 2,
-    img: "https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL200_SR200,200_.jpg",
-    title:
-      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
-    author: "James Clear",
-  },
-  {
-    id: 3,
-    img: "https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg",
-    title: "If Animals Kissed Good Night",
-    author: "Ann Whitford Paul",
-  },
-];
+
+import { books } from "./books";
+import SpecificBook from "./Book";
+import {greeting} from './testing/testing'
+
 function Booklist() {
+  console.log(greeting);
   return (
     <section className="booklist">
       {books.map((book, index) => {
-        return <Book key={book.id} {...book}></Book>;
+        return <SpecificBook key={book.id} {...book}></SpecificBook>;
       })}
     </section>
   );
 }
-
-const Book = ({ img, title, author }) => {
-  // attribute, eventHandler
-  //onClick, onMouseOver
-  const clickHandler = (e) => {
-    console.log(e);
-    console.log(e.target);
-    alert("hello world");
-  };
-  const complexExample = (author) => {
-    console.log(author);
-  };
-  return (
-    <article
-      className="book"
-      onMouseOver={() => {
-        console.log(title);
-      }}
-    >
-      <img src={img} alt="" />
-      <h1 onClick={() => console.log(title)}>{title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler}>
-        reference example
-      </button>
-      <button type="button" onClick={() => complexExample(author)}>
-        more complex example
-      </button>
-    </article>
-  );
-};
 
 ReactDOM.render(<Booklist />, document.getElementById("root"));
